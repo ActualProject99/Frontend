969c734664dfe9cf1dc09bd241bbafff4e1d5540
rklskhj <rklskhj2@naver.com>
2022-11-12 21:29:52 +0900

    미니달력 커스텀 작업

--- src/components/MiniCalendar.tsx
+++ src/components/MiniCalendar.tsx
@@ -1,26 +1,35 @@
 import React, { useState } from "react";
+import setMinutes from "date-fns/setMinutes";
+import setHours from "date-fns/setHours";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { ko } from "date-fns/esm/locale";
+import dayjs from "dayjs";
+
 const MiniCalendar = () => {
-  const [startDate, setStartDate] = useState(new Date());
+  const [startDate, setStartDate] = useState(
+    setHours(setMinutes(new Date(), 0), 20)
+  );
 
-  let handleColor = (time: any) => {
-    return time.getHours() > 12 ? "text-success" : "text-error";
-  };
-
+  console.log("date", Date());
   return (
     <DatePicker
-      className="w[100px] flex justify-center rounded-lg m-auto"
-      showTimeSelect
+      className="w[18rem] flex justify-center rounded-lg m-auto"
       locale={ko}
-      dateFormat="yyyy년 MM월 dd일 (eee)"
+      dateFormat="yyyy년 MM월 dd일 (eee) aa h시mm분 "
       selected={startDate}
       minDate={new Date()}
+      maxDate={new Date()}
       onChange={(date: Date) => setStartDate(date)}
-      timeClassName={handleColor}
+      showTimeSelect
+      timeCaption="공연시간"
+      minTime={setHours(setMinutes(new Date(), 0), 20)}
+      maxTime={setHours(setMinutes(new Date(), 0), 20)}
       disabledKeyboardNavigation
       popperPlacement="auto"
+      // renderCustomHeader={({ date, decreaseMonth, increaseMonth }) => (
+
+      // )}
     />
   );
 };
