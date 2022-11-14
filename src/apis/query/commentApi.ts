70f7f10609f4dcf0a13d69b7706e509b9e57e6a1
Minki Lee <112796300+Pasilda123@users.noreply.github.com>
2022-11-14 11:28:01 +0900

    .

--- src/apis/query/commentApi.ts
+++ src/apis/query/commentApi.ts
@@ -1,13 +1,25 @@
+import { useMutation, useQuery } from '@tanstack/react-query';
 import axios from 'axios'
 import instance, { api } from "../instance/instance";
 
-export const commentList = async (pageNum:number) => {
-    const { data } = await instance.get(`comments?_limit=10&_page=${pageNum}`)
-    return data;
+export const readComments = async ( pageNum:number ) => {
+    const { data } = await instance.get(`comments?_limit=10&_page=${pageNum}`)  
+    return data; // 코멘트를 페이지 당 10개씩 불러오게 지정
 }
 
-export const addComment = async (body: string | number) => {
-    console.log(body);
+export const addComment = async ( body: string | number ) => {
     const { data } = await api.post("comments", body);
     return data.data;
 }
+
+export const removeComment = async ( id:number ) => {
+    const { data } = await api.delete(`comments/${id}`);
+    return data;
+}
+
+export const editComment = async ( id: number , body: string | number ) => {
+    const { data } = await api.patch(`comments/${id}`, body);
+    return data;
+}
+
+
--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -5,7 +5,7 @@ import {
   useQueryClient,
 } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
-import { commentList, addComment } from "../apis/query/commentApi";
+import { readComments, addComment, removeComment } from "../apis/query/commentApi";
 import { useState } from "react";
 import React, { useEffect } from "react"
 
@@ -17,32 +17,45 @@ interface IComments {
 
 const Comment = () => {
   const { id } = useParams();
-  const { mutate } = useMutation(addComment)
-  
+  const { mutate : addCommentFn } = useMutation(addComment);
+  const { mutate : removeCommentFn } = useMutation(removeComment);
   const [currentPage, setCurrentPage] = useState(1);
-  const maxPage = 5;
-  const queryClient = useQueryClient();
+  const maxPage = 5; 
+  /* 10개씩 가져온 코멘트들의 페이지를 최대 5페이지까지로 지정. 
+  *src\apis\query\commentApi.ts 참고. */
 
-  useEffect(()=>{
+  const queryClient = useQueryClient();
+  useEffect(()=>{   
     if(currentPage < maxPage) {
     const nextPage = currentPage + 1;
-    queryClient.prefetchQuery(["allComments", nextPage], () => commentList(nextPage))}
+    queryClient.prefetchQuery(["allComments", nextPage], () => readComments(nextPage))}
   },[currentPage, queryClient]);
+  /* 다음 페이지를 누르기 전에 다음 페이지 내용을 미리 불러옴. 
+  딜레이 체감 없애기 */
 
   const { isLoading, isError, data } = useQuery<IComments[]>(
     ["allComments", currentPage],
-    () => commentList(currentPage),
+    
+    () => readComments(currentPage),
     {
       staleTime:2000,
       keepPreviousData: true,
     }
   );
-  if (isLoading) return <h3>Loading...</h3>;
-  if (isError) return <h3>지금은 댓글을 불러올 수 없어요!</h3>;
   
+  
+  if (isLoading) {
+    return <h3>Loading...</h3>; 
+  }
+  if (isError) {
+    return <h3>지금은 댓글을 불러올 수 없어요!</h3>;
+  }
+
   const onSubmit = (e:React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
   }
+
+  
 
   return (
     <div>
@@ -58,6 +71,7 @@ const Comment = () => {
             <li key={comment.id}>
               {comment.comment}
               <p>2022.11.11 22:59</p>
+              <button onClick={()=>{}}>삭제</button>
             </li>
           ))}
       </ul>
