b2b9908807d0318644d89386052db4d7f4de5ac3
Minki Lee <112796300+Pasilda123@users.noreply.github.com>
2022-11-23 01:55:24 +0900

    .

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -9,25 +9,64 @@ interface Concert {
   startDatetime: string;
   endDatetime: string;
 }
-const Cards = ({ concert, data }: { concert?: boolean; data: Concert }) => {
+interface Props {
+  concert?: boolean;
+  data: Concert;
+  horizontal?: boolean;
+  vertical?: boolean;
+}
+
+const Cards = ({ concert, data, horizontal, vertical, ...rest }: Props) => {
   const navigate = useNavigate();
   const handleClick = () => {
     navigate(`./${data.id}`);
   };
-  return concert ? (
-    <div className="p-6 border w-56 h-96 cursor-pointer" onClick={handleClick}>
-      <img className="w-[180px] h-[250px] mx-auto" src={data.imageUrl} alt="" />
-      <p className="whitespace-nowrap overflow-hidden text-ellipsis text-base font-bold">
-        {data.name}
-      </p>
-      {/* <p className="text-xs">
+  return (
+    <>
+      {concert && vertical ? (
+        <div
+          className="p-6 border w-56 h-96 cursor-pointer"
+          onClick={handleClick}
+        >
+          <img
+            className="w-[180px] h-[250px] mx-auto"
+            src={data.imageUrl}
+            alt=""
+          />
+          <p className="whitespace-nowrap overflow-hidden text-ellipsis text-base font-bold">
+            {data.name}
+          </p>
+          {/* <p className="text-xs">
         {format(parseISO(data.startDatetime), "yyyy-MM-dd") +
           " ~ " +
           format(parseISO(data.startDatetime), "yyyy-MM-dd")}
       </p> */}
-      <p className="text-xs">{data.location}</p>
-    </div>
-  ) : null;
+          <p className="text-xs">{data.location}</p>
+        </div>
+      ) : null}
+      {concert && horizontal ? (
+        <div
+          className="p-6 border w-56 h-96 cursor-pointer"
+          onClick={handleClick}
+        >
+          <img
+            className="w-[180px] h-[250px] mx-auto"
+            src={data.imageUrl}
+            alt=""
+          />
+          <p className="whitespace-nowrap overflow-hidden text-ellipsis text-base font-bold">
+            {data.name}
+          </p>
+          {/* <p className="text-xs">
+        {format(parseISO(data.startDatetime), "yyyy-MM-dd") +
+          " ~ " +
+          format(parseISO(data.startDatetime), "yyyy-MM-dd")}
+      </p> */}
+          <p className="text-xs">{data.location}</p>
+        </div>
+      ) : null}
+    </>
+  );
 };
 
 export default Cards;
