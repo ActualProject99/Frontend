da1b8ba7d4c08e86eca961059f8baa228234992f
obov <obov3e@gmail.com>
2022-11-25 16:21:06 +0900

    arrange

--- src/pages/Concerts.tsx
+++ src/pages/Concerts.tsx
@@ -53,105 +53,110 @@ const Concerts = ({ no1, no2 }: { no1?: boolean; no2?: boolean }) => {
   }, [select, dateChosen]);
   const [isVisible, setIsVisible] = useState(false);
   return (
-    <div className="pt-16 mb-8  h-[460px]">
+    <>
       <ConcertSlider />
-      <div className="max-w-md px-4 mx-auto sm:px-7 md:max-w-4xl md:px-6 h-full">
-        <div className="md:grid md:grid-cols-2 md:divide-x md:divide-gray-200  h-full relative">
-          <div className="relative h-full">
-            {isMd ? (
-              <div className="w-80" ref={fixsolute} style={fixoluteStyle}>
-                <Calendar selectable checkedDates={getdateAllConcerts} />
-                <ul className="flex justify-center gap-3 flex-wrap">
-                  {groups.map((group, i) => (
-                    <li
-                      key={group}
+      <div className="pt-16 mb-8  h-[460px]">
+        <div className="max-w-md px-4 mx-auto sm:px-7 md:max-w-4xl md:px-6 h-full">
+          <div className="md:grid md:grid-cols-2 md:divide-x md:divide-gray-200  h-full relative">
+            <div className="relative h-full">
+              {isMd ? (
+                <div className="w-80" ref={fixsolute} style={fixoluteStyle}>
+                  <Calendar selectable checkedDates={getdateAllConcerts} />
+                  <ul className="flex justify-center gap-3 flex-wrap">
+                    {groups.map((group, i) => (
+                      <li
+                        key={group}
+                        className={cls(
+                          "px-3 py-1 rounded-full cursor-pointer flex items-center justify-center font-bold border transition-colors",
+                          i === select && "bg-primary-main text-white"
+                        )}
+                        onClick={handleClick(i)}
+                      >
+                        {group}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              ) : (
+                <>
+                  <Portal>
+                    <button
                       className={cls(
-                        "px-3 py-1 rounded-full cursor-pointer flex items-center justify-center font-bold border transition-colors",
-                        i === select && "bg-primary-main text-white"
+                        "fixed top-1/4 transition-all",
+                        isVisible ? "-left-16" : "left-0"
                       )}
-                      onClick={handleClick(i)}
+                      onClick={() => {
+                        setIsVisible((cur) => !cur);
+                      }}
                     >
-                      {group}
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ) : (
-              <>
-                <Portal>
-                  <button
-                    className={cls(
-                      "fixed top-1/4 transition-all",
-                      isVisible ? "-left-16" : "left-0"
-                    )}
-                    onClick={() => {
-                      setIsVisible((cur) => !cur);
-                    }}
-                  >
-                    <CalenderDrawer />
-                  </button>
-                </Portal>
-                {isVisible ? (
-                  <Modal
-                    onClick={() => {
-                      setIsVisible((cur) => !cur);
-                    }}
-                  >
-                    <div className="fixed left-0 top-0">
-                      <Calendar selectable checkedDates={getdateAllConcerts} />
-                      <ul className="flex justify-center gap-3 flex-wrap mt-12 md:px-8">
-                        {groups.map((group, i) => (
-                          <li
-                            key={group}
-                            className={cls(
-                              "px-3 py-1 rounded-full cursor-pointer flex items-center justify-center font-bold border transition-colors",
-                              i === select && "bg-primary-main text-white"
-                            )}
-                            onClick={handleClick(i)}
-                          >
-                            {group}
-                          </li>
-                        ))}
-                      </ul>
-                    </div>
-                  </Modal>
-                ) : null}
-              </>
-            )}
-          </div>
-          <section
-            ref={limit}
-            className="mt-12 md:min-h-[700px] md:mt-0 md:pl-8 flex flex-col justify-center items-center gap-10 md:col-span-2"
-          >
-            {/* <h2 className="font-semibold text-gray-900">
+                      <CalenderDrawer />
+                    </button>
+                  </Portal>
+                  {isVisible ? (
+                    <Modal
+                      onClick={() => {
+                        setIsVisible((cur) => !cur);
+                      }}
+                    >
+                      <div className="fixed left-0 top-0">
+                        <Calendar
+                          selectable
+                          checkedDates={getdateAllConcerts}
+                        />
+                        <ul className="flex justify-center gap-3 flex-wrap mt-12 md:px-8">
+                          {groups.map((group, i) => (
+                            <li
+                              key={group}
+                              className={cls(
+                                "px-3 py-1 rounded-full cursor-pointer flex items-center justify-center font-bold border transition-colors",
+                                i === select && "bg-primary-main text-white"
+                              )}
+                              onClick={handleClick(i)}
+                            >
+                              {group}
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                    </Modal>
+                  ) : null}
+                </>
+              )}
+            </div>
+            <section
+              ref={limit}
+              className="mt-12 md:min-h-[700px] md:mt-0 md:pl-8 flex flex-col justify-center items-center gap-10 md:col-span-2"
+            >
+              {/* <h2 className="font-semibold text-gray-900">
                 Schedule for{" "}
                 <time dateTime={format(dateChosen, "yyyy-MM-dd")}>
                   {format(dateChosen, "MMM dd, yyy")}
                 </time>
               </h2> */}
 
-            <div className="hidden xl:flex gap-4 justify-center mt-12 flex-wrap">
-              {getShowingConcerts.length > 0 ? (
-                getShowingConcerts.map((consert, i) => (
-                  <Cards key={i} vertical concert data={consert} />
-                ))
-              ) : (
-                <p>No concerts for today.</p>
-              )}
-            </div>
-            <div className="flex xl:hidden gap-2 justify-center mt-12 flex-wrap">
-              {getShowingConcerts.length > 0 ? (
-                getShowingConcerts.map((consert, i) => (
-                  <Cards key={i} horizontal concert data={consert} />
-                ))
-              ) : (
-                <p>No concerts for today.</p>
-              )}
-            </div>
-          </section>
+              <div className="hidden xl:flex gap-4 justify-center mt-12 flex-wrap">
+                {getShowingConcerts.length > 0 ? (
+                  getShowingConcerts.map((consert, i) => (
+                    <Cards key={i} vertical concert data={consert} />
+                  ))
+                ) : (
+                  <p>No concerts for today.</p>
+                )}
+              </div>
+              <div className="flex xl:hidden gap-2 justify-center mt-12 flex-wrap">
+                {getShowingConcerts.length > 0 ? (
+                  getShowingConcerts.map((consert, i) => (
+                    <Cards key={i} horizontal concert data={consert} />
+                  ))
+                ) : (
+                  <p>No concerts for today.</p>
+                )}
+              </div>
+            </section>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
