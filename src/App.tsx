2d69a16856d1f98438e7cce28c63e7a2b0d19bcc
obov <obov3e@gmail.com>
2022-11-25 14:27:03 +0900

    delete production console

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
+if (process.env.NODE_ENV === "production") {
+  console.log = function no_console() {};
+  console.warn = function no_console() {};
+}
 function App() {
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
