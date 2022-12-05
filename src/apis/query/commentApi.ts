cf959c37fe19baed35fdd734ea5b77296e80c8a1
Minki Lee <112796300+Pasilda123@users.noreply.github.com>
2022-12-05 17:44:05 +0900

    .

--- src/apis/query/commentApi.ts
+++ src/apis/query/commentApi.ts
@@ -1,8 +1,6 @@
 import { deactivate, activate } from "../instance";
 import { IPayload } from '../../types';
 
-
-
 export const readComments = async ( concertId: IPayload) => {
   const { data } = await deactivate.get(`comment/${concertId}`);
   return data;
