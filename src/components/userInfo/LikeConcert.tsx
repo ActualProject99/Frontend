5997657f13c2906bd638e128efec3b6435454852
rklskhj <rklskhj2@naver.com>
2022-12-08 13:51:49 +0900

    달력 버그 해결

--- src/components/userInfo/LikeConcert.tsx
+++ src/components/userInfo/LikeConcert.tsx
@@ -40,18 +40,18 @@ const LikeConcert = ({ concert }: IConcertProps): JSX.Element => {
   }, [poped]);
 
   return (
-    <div className="grid w-56 h-[26rem] group">
-      <div className="flex justify-center m-auto w-56 h-[18rem] overflow-hidden ">
+    <div className="grid w-56 h-[26rem] group relative">
+      <div className="flex justify-center m-auto w-52 h-72 overflow-hidden ">
         <img
-          className="object-contain cursor-pointer transition-all ease-in-out duration-700 hover:scale-110"
+          className=" h-72 cursor-pointer"
           alt="poster"
           onClick={() => navigate(`/concerts/${concert.concertId}`)}
           src={concert.concertImg}
         />
       </div>
       <div className="flex flex-col justify-center items-center p-1 ">
-        {concert.concertName.length < 11 ? (
-          <p className="flex justify-center w-48 font-bold ">
+        {concert.concertName.length < 14 ? (
+          <p className="flex justify-center w-48 font-bold">
             {concert.concertName}
           </p>
         ) : (
@@ -77,6 +77,15 @@ const LikeConcert = ({ concert }: IConcertProps): JSX.Element => {
             />
           )}
         </div>
+        {concert.saleDone === "판매완료" ? (
+          <p className="flex items-center justify-center absolute top-3 right-0 w-16 h-5 text-sm text-white font-bold rounded bg-slate-500 mr-3">
+            {concert.saleDone}
+          </p>
+        ) : (
+          <p className="flex items-center justify-center absolute top-3 right-1 w-16 h-5 text-sm text-white font-bold rounded bg-accent-main mr-2">
+            {concert.saleDone}
+          </p>
+        )}
       </div>
       <Ticket />
     </div>
