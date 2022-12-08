6c53d25ae7ff4f5fc5b7399517fce530a215ff6c
rklskhj <rklskhj2@naver.com>
2022-12-09 04:01:48 +0900

    dev pull commit

--- src/apis/query/OAuthApi.ts
+++ src/apis/query/OAuthApi.ts
@@ -1,12 +1,14 @@
+//@ts-nocheck
 import { setAccessToken } from "../cookie";
 import { activate, deactivate } from "../instance";
 
 export const OAuthAPI = {
   loginWithKakao: async (kakaoToken: string) => {
+    console.log("여기옴?");
     return await deactivate
       .get(`/users/oauth/kakao/callback?code=${kakaoToken}`) //백엔드 리다이렉트 주소로 맞추기
       .then((data) => {
-        console.log("data", data);
+        console.log("뭐주냐?", data);
         setAccessToken("data.headers.authorization");
       })
       .then(() => {
--- src/components/login/OAuthKakao.tsx
+++ src/components/login/OAuthKakao.tsx
@@ -1,19 +1,70 @@
 import { useEffect } from "react";
 import { OAuthAPI } from "../../apis/query/OAuthApi";
+import axios from "axios";
+import { setAccessToken } from "../../apis/cookie";
+import { deactivate } from "../../apis/instance";
 
 const OAuthKakao = () => {
   const kakaoToken = new URL(window.location.href).searchParams.get("code");
   useEffect(() => {
-    try {
-      if (kakaoToken) {
-        OAuthAPI.loginWithKakao(kakaoToken);
+    (async () => {
+      try {
+        const kakaoResult = await axios.post(
+          `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${process.env.REACT_APP_REST_API_KEY}&redirect_uri=${process.env.REACT_APP_REDIRECT_FRONT}&code=${kakaoToken}`,
+          {
+            headers: {
+              "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+            },
+          }
+        );
+        console.log("카카오답변", kakaoResult);
+        if (kakaoResult.status !== 200) return;
+        const token = kakaoResult.data.access_token;
+        console.log("토큰", token);
+        const response = await deactivate.post(
+          `users/oauth/kakao/callback`,
+          kakaoResult.data,
+          {
+            headers: {
+              Authorization: token,
+            },
+          }
+        );
+
+        const {
+          status,
+          data: { accessToken, refreshToken, currentPage },
+        } = response;
+        console.log("res", response);
+        if (status !== 200) return;
+        setAccessToken(accessToken);
+        localStorage.setItem("token", refreshToken);
+        if (currentPage) {
+          return window.location.replace(`/${currentPage}`);
+        } else {
+          return window.location.replace("/concerts");
+        }
+      } catch (e) {
+        console.log("에러");
+        console.error(e);
+        // window.location.replace("/");
+        //알러트
       }
-    } catch (err) {
-      window.location.href = "/";
-      return;
-    }
+    })();
   }, [kakaoToken]);
   return <></>;
 };
 
 export default OAuthKakao;
+
+// try {
+//     if (kakaoToken) {
+//       console.log("됨?");
+//       OAuthAPI.loginWithKakao(kakaoToken);
+//     }
+//   } catch (err) {
+//     // window.location.href = "/";
+//     return;
+//   }
+// }, [kakaoToken]);
+// return <></>;
